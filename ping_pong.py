0e33f68bf7c0ed449fab424bfb29806cf489b6d1
nehirbcogl <107806843+nehirbcogl@users.noreply.github.com>
2022-06-26 20:09:11 +0300

    Update ping_pong.py
    v 2.0
    karakter ve oyuncu sınıfı oluşturuldu
    raketler ve top ekrana yansıtıldı

--- ping_pong.py
+++ ping_pong.py
@@ -1,5 +1,44 @@
 from pygame import *
 
+class Karakter(sprite.Sprite):
+    def __init__(self, krk_image, krk_x, krk_y, krk_hiz, genislik, yukseklik):
+        super().__init__()
+        self.image = transform.scale(image.load(krk_image),(genislik,yukseklik))
+        self.hiz = krk_hiz
+
+        self.rect = self.image.get_rect()
+        self.rect.x = krk_x
+        self.rect.y = krk_y
+
+    def ciz(self):
+        pencere.blit(self.image,(self.rect.x,self.rect.y))
+
+
+#raketlerin oluşturulması
+class Oyuncu(Karakter):
+    def update_r(self):
+        basilan_tuslar = key.get_pressed()
+        if basilan_tuslar[K_UP] and self.rect.y >5:
+            self.rect.x -= self.hiz
+        if basilan_tuslar[K_DOWN] and self.rect.y < 420:
+            self.rect.x += self.hiz 
+
+    def update_l(self):
+        basilan_tuslar = key.get_pressed()
+        if basilan_tuslar[K_w] and self.rect.y >5:
+            self.rect.x -= self.hiz
+        if basilan_tuslar[K_s] and self.rect.y < 420:
+            self.rect.x += self.hiz 
+
+
+raket_sol = Oyuncu("racket.png",30,200,4,50,150)
+raket_sag = Oyuncu("racket.png",520,200,4,50,150)
+
+top = Karakter("tenis_ball.png",200,200,4,50,50)
+
+
+
+
 #oyun penceresi oluşturma
 pencere = display.set_mode((600,500))
 #pencere arkaplan rengi RGB kodu
@@ -16,6 +55,14 @@ while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
+
+
+    raket_sag.ciz()
+    raket_sol.ciz()
+    top.ciz()
+
+    raket_sol.update_l()
+    raket_sag.update_r()
 
     #ekranı güncelle
     display.update()
